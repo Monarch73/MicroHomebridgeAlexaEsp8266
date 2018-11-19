b0d8f48ba336170faeffd2d82e7536c028e200ce
Niels Hüsken <niels.huesken@ax-ao.de>
2018-11-19 11:18:29 +0100

    Initial Commit

---
+++ MicroHomebridgeAlexaEsp8266/__vm/.MicroHomebridgeAlexaEsp8266.vsarduino.h
@@ -0,0 +1,96 @@
+/* 
+	Editor: https://www.visualmicro.com/
+			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: NodeMCU 1.0 (ESP-12E Module), Platform=Arduino, Package=esp8266
+*/
+
+#if defined(_VMICRO_INTELLISENSE)
+
+#ifndef _VSARDUINO_H_
+#define _VSARDUINO_H_
+#define __ARDUINO_ESp8266__
+#define __ARDUINO_ESP8266__
+#define __ets__
+#define ICACHE_FLASH
+#define F_CPU 80000000L
+#define LWIP_OPEN_SRC
+#define TCP_MSS 536
+#define ARDUINO 10801
+#define ARDUINO_ESP8266_NODEMCU
+#define ARDUINO_ARCH_ARDUINO
+#define ESP8266
+#define __cplusplus 201103L
+#define __inline__
+#define __asm__(x)
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __volatile__
+
+
+#define __ICCARM__
+#define __ASM
+#define __INLINE
+#define __builtin_va_list void
+//#define _GNU_SOURCE 
+//#define __GNUC__ 0
+//#undef  __ICCARM__
+//#define __GNU__
+
+typedef long Pio;
+typedef long Efc;
+typedef long Adc;
+typedef long Pwm;
+typedef long Rtc;
+typedef long Rtt;
+typedef long pRtc;
+typedef long Spi;
+typedef long spi;
+typedef long Ssc;
+//typedef long p_scc;
+typedef long Tc;
+//typedef long pTc;
+typedef long Twi;
+typedef long Wdt;
+//typedef long pTwi;
+typedef long Usart;
+typedef long Pdc;
+typedef long Rstc;
+
+extern const int ADC_MR_TRGEN_DIS = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG0 = 0;
+extern const int ADC_MR_TRGSEL_Pos = 0;
+
+extern const int ADC_MR_TRGSEL_Msk = 0;
+extern const int ADC_MR_TRGEN = 0;
+extern const int ADC_TRIG_TIO_CH_0 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG1 = 0;
+extern const int ADC_TRIG_TIO_CH_1 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG2 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG3 = 0;
+
+#define __ARMCC_VERSION 400678
+#define __attribute__(noinline)
+
+#define prog_void
+#define PGM_VOID_P int
+
+
+            
+typedef unsigned char byte;
+extern "C" void __cxa_pure_virtual() {;}
+
+
+
+#include <arduino.h>
+#include <pins_arduino.h> 
+#undef cli
+#define cli()
+#include "MicroHomebridgeAlexaEsp8266.ino"
+#endif
+#endif
