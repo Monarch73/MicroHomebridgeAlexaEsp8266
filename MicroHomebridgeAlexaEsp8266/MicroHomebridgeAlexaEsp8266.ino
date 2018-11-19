598acbcfb0445a3011d600b23f4d5e5b88f8b9b8
Niels Hüsken <niels.huesken@ax-ao.de>
2018-11-19 10:58:10 +0100

    Add project files.

---
+++ MicroHomebridgeAlexaEsp8266/MicroHomebridgeAlexaEsp8266.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MicroHomebridgeAlexaEsp8266.ino
+ Created:	11/19/2018 10:58:03 AM
+ Author:	niels.huesken
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
